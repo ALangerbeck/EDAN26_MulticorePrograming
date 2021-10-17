ecc1bc65151a829cd6ea4eb608cd1f54f0d1b020
JohnGregory <98alla02@gmail.com>
2021-10-17 14:55:31 +0200

    added lab6 matrix mult

--- lab6/mmp.c
+++ lab6/mmp.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <omp.h>
 
 #define N (2048)
+#define nthread ()
 
 float sum;
 float a[N][N];
@@ -12,6 +14,8 @@ void matmul()
 {
 	int	i, j, k;
 
+	#pragma omp parallel private(i,j,k)
+	#pragma omp for schedule(static, N/omp_get_num_procs())
 	for (i = 0; i < N; i += 1) {
 		for (j = 0; j < N; j += 1) {
 			a[i][j] = 0;
@@ -26,6 +30,8 @@ void init()
 {
 	int	i, j;
 
+	#pragma omp parallel private(i,j)
+	#pragma omp for schedule(static, N/omp_get_num_procs())
 	for (i = 0; i < N; i += 1) {
 		for (j = 0; j < N; j += 1) {
 			b[i][j] = 12 + i * j * 13;
@@ -37,15 +43,21 @@ void init()
 void check()
 {
 	int	i, j;
+	sum = 0;
 
+	#pragma parallel private(i,j)
+	#pragma omp for reduction(+:sum)
 	for (i = 0; i < N; i += 1)
 		for (j = 0; j < N; j += 1)
 			sum += a[i][j];
+
 	printf("sum = %lf\n", sum);
+	printf("diff = %lf\n", sum -2672835994823558168576.000000);
 }
 
 int main()
 {	
+	omp_set_num_threads(omp_get_num_procs());
 	init();
 	matmul();
 	check();
